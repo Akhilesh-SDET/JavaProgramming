9d6969ac46cb39dd9a1d2096d3b2158af2379d3d
Akhilesh Kumar <akhilesh.kumar95@outlook.com>
2026-02-28 17:57:33 +0530

    fourthCommit

--- src/interviewBasedJavaProgramming/A0001.java
+++ src/interviewBasedJavaProgramming/A0001.java
@@ -5,5 +5,6 @@ public class A0001 {
 		System.out.println("commit first");
 		System.out.println("commit second");
 		System.out.println("commit third");
+		System.out.println("commit four");
 	}
 }
