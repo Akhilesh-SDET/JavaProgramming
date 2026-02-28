0bffd9a9330644b363a5e929d4d2fa405f166b11
Akhilesh Kumar <akhilesh.kumar95@outlook.com>
2026-02-28 17:56:22 +0530

    ThirdCommit

--- src/interviewBasedJavaProgramming/A0001.java
+++ src/interviewBasedJavaProgramming/A0001.java
@@ -4,5 +4,6 @@ public class A0001 {
 	public static void main(String[] args) {
 		System.out.println("commit first");
 		System.out.println("commit second");
+		System.out.println("commit third");
 	}
 }
