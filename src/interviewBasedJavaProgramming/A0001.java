7771ff78e08710096eb59f8df59cba00fd064778
Akhilesh Kumar <akhilesh.kumar95@outlook.com>
2026-02-28 17:55:19 +0530

    secondCommit

--- src/interviewBasedJavaProgramming/A0001.java
+++ src/interviewBasedJavaProgramming/A0001.java
@@ -3,5 +3,6 @@ package interviewBasedJavaProgramming;
 public class A0001 {
 	public static void main(String[] args) {
 		System.out.println("commit first");
+		System.out.println("commit second");
 	}
 }
